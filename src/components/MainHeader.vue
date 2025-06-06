f8ce0b514408ec3d081e3f7f11184f5799d3e808
Alex <alex@example.com>
2025-06-06 17:09:49 +0300

    first commit

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,121 @@
+<script setup></script>
+
+<template>
+	<header class="header">
+		<div class="container">
+			<nav class="menu">
+				<div class="title">
+					<h1>
+						OLYMPIANS<br />
+						BOOKS
+					</h1>
+				</div>
+				<div class="arrow">
+					<img
+						src="/src/assets/images/header/arrow-down.svg"
+						alt="arrow-down"
+					/>
+				</div>
+				<div class="shopping">
+					<img
+						src="/src/assets/images/header/shopping-cart.svg"
+						alt="shopping-cart"
+					/>
+				</div>
+			</nav>
+			<div class="description">
+				<div class="subtitle">
+					КНИГИ, КОТОРЫЕ<br />
+					ВОЗНЕСУТ ВАС НА<br />
+					ОЛИМП
+				</div>
+			</div>
+			<div class="more">
+				<button>УЗНАТЬ БОЛЬШЕ</button>
+			</div>
+		</div>
+	</header>
+</template>
+
+<style scoped>
+.header {
+	height: 940px;
+	width: 1440px;
+	background-image: url(/src/assets/images/header/Hero_background.png);
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: center;
+	margin: 0 auto;
+	display: flex;
+	flex-direction: column;
+	color: white;
+}
+
+.container {
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	margin: 0;
+	padding-left: 70px;
+	padding-right: 74px;
+}
+
+.menu {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	height: 100px;
+}
+
+.title {
+	font-family: 'Montserrat', sans-serif;
+	font-size: 24px;
+	font-weight: 700;
+	letter-spacing: 0.1em;
+	text-transform: uppercase;
+}
+
+.arrow {
+	width: 64px;
+	height: 18px;
+}
+
+.shopping {
+	width: 55px;
+	height: 64px;
+}
+
+.description {
+	display: flex;
+	flex-direction: column;
+	justify-content: start;
+	height: 460px;
+	width: 860px;
+	margin-top: 20px;
+	padding-top: 77px;
+	padding-bottom: 77px;
+}
+
+.subtitle {
+	font-size: 80px;
+	line-height: 120px;
+	letter-spacing: 0.07em;
+}
+
+.more button {
+	margin-top: 20px;
+	width: 420px;
+	height: 75px;
+	font-size: 42px;
+	line-height: 1.2px;
+	letter-spacing: 0.07em;
+	background: transparent;
+	color: white;
+	border: 2px solid white;
+}
+
+.more button:active {
+	background: white;
+	color: black;
+}
+</style>
