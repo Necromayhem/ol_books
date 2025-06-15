8ddcc1b51e717643405e4369f4c1c921540a652b
Alex <alex@example.com>
2025-06-15 11:41:16 +0300

    add cart

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -25,39 +25,56 @@ const removeFromCart = item => {
 			<div class="title">
 				<h2>Корзина</h2>
 			</div>
-			<div class="info">
-				<div class="item">Название товара</div>
-				<div class="price">Цена за шт.</div>
-				<div class="quantity">Количество</div>
-				<div class="total-price">Общая цена</div>
+			<div v-if="cartStore.items.length === 0" class="empty_cart">
+				<span
+					>Ваша корзина пока пуста. Отыщите в библиотеке необходимые
+					фолианты.</span
+				>
 			</div>
-			<div class="cart-wrapper">
-				<div class="cart-list">
-					<div class="cart-item" v-for="item in cartStore.items" :key="item.id">
-						<div class="title-list">
-							<img :src="item.image" alt="book.jpg" />
-							<div>{{ item.author }}, {{ item.title }}</div>
-						</div>
-						<div class="price-list">
-							<div>{{ item.price }} р.</div>
-						</div>
-						<div class="quantity-list">
-							<button class="btn_remove" @click="deleteItem(item.id)">
-								<img src="/src/assets/images/cart/minus.svg" alt="" />
+			<div v-else>
+				<div class="info">
+					<div class="item">Название товара</div>
+					<div class="price">Цена за шт.</div>
+					<div class="quantity">Количество</div>
+					<div class="total-price">Общая цена</div>
+				</div>
+				<div class="cart-wrapper">
+					<div class="cart-list">
+						<div
+							class="cart-item"
+							v-for="item in cartStore.items"
+							:key="item.id"
+						>
+							<div class="title-list">
+								<img :src="item.image" alt="book.jpg" />
+								<div>{{ item.author }}, “{{ item.title }}”</div>
+							</div>
+							<div class="price-list">
+								<div>{{ item.price }} р.</div>
+							</div>
+							<div class="quantity-list">
+								<button class="btn_remove" @click="deleteItem(item.id)">
+									<img src="/src/assets/images/cart/minus.svg" alt="" />
+								</button>
+								<span>{{ item.quantity }} шт.</span>
+								<button class="btn_add" @click="addToCart(item)">
+									<img src="/src/assets/images/cart/plus.svg" alt="" />
+								</button>
+							</div>
+							<div class="total-price-list">
+								<div>{{ item.price * item.quantity }} р.</div>
+							</div>
+							<button class="remove-list" @click="removeFromCart(item.id)">
+								<img src="/src/assets/images/cart/remove.svg" alt="" />
 							</button>
-							<span>{{ item.quantity }} шт.</span>
-							<button class="btn_add" @click="addToCart(item)">
-								<img src="/src/assets/images/cart/plus.svg" alt="" />
-							</button>
 						</div>
-						<div class="total-price-list">
-							<div>{{ item.price * item.quantity }} р.</div>
-						</div>
-						<button class="remove-list" @click="removeFromCart(item.id)">
-							<img src="/src/assets/images/cart/remove.svg" alt="" />
-						</button>
 					</div>
 				</div>
+				<div class="total">
+					<span class="for_payment">Итого к оплате</span>
+					<span class="pay">{{ cartStore.totalPrice }} р.</span>
+				</div>
+				<div class="order"><button>Оформить заказ</button></div>
 			</div>
 		</main>
 		<Footer />
@@ -144,6 +161,7 @@ const removeFromCart = item => {
 }
 
 .cart-item {
+	margin-top: 20px;
 	width: 100%;
 	max-width: 1300px;
 	height: 220px;
@@ -151,6 +169,7 @@ const removeFromCart = item => {
 	justify-content: space-between;
 	padding-left: 55px;
 	padding-right: 29px;
+	border: 3px solid black;
 
 	.title-list,
 	.price-list,
@@ -172,22 +191,18 @@ const removeFromCart = item => {
 	.title-list div {
 		margin-right: 20px;
 		display: flex;
-		max-width: 310px;
-		max-height: 220px;
-		width: 100%;
-		height: 100%;
+		flex-wrap: wrap;
+		width: 310px;
+		height: 220px;
+		text-align: left;
 		align-items: center;
-		justify-content: center;
-		text-align: center;
 	}
 
 	.price-list div {
 		margin-right: 20px;
 		display: flex;
-		max-width: 200px;
-		max-height: 75px;
-		width: 100%;
-		height: 100%;
+		width: 200px;
+		height: 75px;
 		align-items: center;
 		justify-content: center;
 		text-align: center;
@@ -197,9 +212,8 @@ const removeFromCart = item => {
 		margin-right: 20px;
 		display: flex;
 		align-items: center;
-		justify-content: space-between; // Добавляем сюда
-		max-width: 200px;
-		width: 100%;
+		justify-content: space-between;
+		width: 200px;
 
 		button {
 			display: flex;
@@ -216,10 +230,8 @@ const removeFromCart = item => {
 	.total-price-list div {
 		margin-right: 49px;
 		display: flex;
-		max-width: 200px;
-		max-height: 75px;
-		width: 100%;
-		height: 100%;
+		width: 200px;
+		height: 75px;
 		align-items: center;
 		justify-content: center;
 		text-align: center;
@@ -239,6 +251,62 @@ const removeFromCart = item => {
 .remove-list img {
 	width: 32px;
 	height: 32px;
+}
+
+.total {
+	margin-top: 20px;
+	color: black;
+	font-family: 'Forum', sans-serif;
+	font-size: 42px;
+	line-height: 1.5;
+	letter-spacing: 0.07em;
+	display: flex;
+	justify-content: space-between;
+	padding-left: 100px;
+	padding-right: 110px;
+	.for_payment {
+		text-transform: uppercase;
+	}
+}
+
+.order {
+	margin-top: 20px;
+	margin-bottom: 140px;
+	display: flex;
+	width: 100%;
+	max-width: 1300px;
+	justify-content: center;
+	button {
+		width: 640px;
+		height: 100px;
+		font-family: 'Forum', sans-serif;
+		font-size: 42px;
+		line-height: 1.5;
+		letter-spacing: 0.07em;
+		text-transform: uppercase;
+		border: 3px solid black;
+		transition:
+			background-color 0.2s ease,
+			color 0.2s ease;
+
+		&:active {
+			background-color: #1596c1;
+			color: black;
+		}
+	}
+}
+
+.empty_cart {
+	@include main_text;
+	color: black;
+	width: 100%;
+	max-width: 1300px;
+	height: 100px;
+	display: flex;
+	text-align: center;
+	align-items: center;
+	justify-content: center;
+	margin-top: 20px;
 }
 
 .footer {
