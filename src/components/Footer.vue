11e97aaa7980a5f7b651d0656da35aa1a7595b26
Alex <alex@example.com>
2025-06-15 15:23:26 +0300

    add underlining footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,36 @@
-<script setup></script>
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
 
 <template>
 	<section class="footer">
 		<div class="container">
-			<router-link to="/"
-				><div class="logo">
+			<router-link to="/">
+				<div class="logo">
 					OLYMPIANS<br />
 					BOOKS
-				</div></router-link
-			>
+				</div>
+			</router-link>
 			<div class="phone">+30922040281</div>
-			<div class="main"><router-link to="/">Главная</router-link></div>
-			<div class="catalog">
-				<router-link to="/catalog">Каталог</router-link>
+			<div class="main">
+				<router-link to="/" :class="{ active: route.path === '/' }"
+					>Главная</router-link
+				>
 			</div>
-			<div class="cart"><router-link to="/cart">Корзина</router-link></div>
+			<div class="catalog">
+				<router-link
+					to="/catalog"
+					:class="{ active: route.path === '/catalog' }"
+					>Каталог</router-link
+				>
+			</div>
+			<div class="cart">
+				<router-link to="/cart" :class="{ active: route.path === '/cart' }"
+					>Корзина</router-link
+				>
+			</div>
 		</div>
 	</section>
 </template>
@@ -48,39 +64,92 @@
 .phone {
 	@include main_text;
 	color: #ffffff;
-	width: 310px;
+	width: fit-content; /* Подстраивается под текст */
 	height: 37px;
 	margin-right: 20px;
+	cursor: pointer;
+	position: relative;
+	display: inline-block; /* Чтобы подчёркивание было по ширине текста */
+
+	&::after {
+		content: '';
+		position: absolute;
+		left: 0;
+		bottom: -5px;
+		width: 0;
+		height: 2px;
+		background-color: white;
+		transition:
+			width 0.3s ease,
+			background-color 0.3s ease;
+	}
+
+	&:hover {
+		color: white;
+	}
+
+	&:hover::after {
+		width: 100%;
+	}
+
+	&:active {
+		color: black;
+	}
+
+	&:active::after {
+		width: 100%;
+		background-color: black;
+	}
 }
 
-.main {
-	@include main_text;
-	color: #ffffff;
-	width: 200px;
-	height: 37px;
-	margin-right: 20px;
-	text-align: center;
-}
-
-.catalog {
-	@include main_text;
-	color: #ffffff;
-	width: 200px;
-	height: 37px;
-	margin-right: 20px;
-	text-align: center;
-}
-
+.main,
+.catalog,
 .cart {
 	@include main_text;
 	color: #ffffff;
 	width: 200px;
 	height: 37px;
+	margin-right: 20px;
 	text-align: center;
-}
 
-.router-link-active {
-	background: transparent !important;
-	color: inherit !important;
+	a {
+		color: inherit;
+		text-decoration: none;
+		position: relative;
+		display: inline-block;
+		width: 100%;
+		height: 100%;
+		line-height: 37px;
+
+		&::after {
+			content: '';
+			position: absolute;
+			left: 0;
+			bottom: -5px;
+			width: 0;
+			height: 2px;
+			background-color: white;
+			transition:
+				width 0.3s ease,
+				background-color 0.3s ease;
+		}
+
+		&:hover {
+			color: white;
+		}
+
+		&:hover::after {
+			width: 100%;
+		}
+
+		&:active {
+			color: black;
+		}
+
+		&:active::after {
+			width: 100%;
+			background-color: black;
+		}
+	}
 }
 </style>
